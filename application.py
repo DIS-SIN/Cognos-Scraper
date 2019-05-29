973ef0e1c8d2be8d6b0c3103ab000197f097be89
unknown <38829219+deepklim@users.noreply.github.com>
2019-05-29 14:48:40 -0400

    Disable pushing to DB while getting set up

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
 from comments_scraper.src import download_files
 from comments_scraper.src import munge_comments
 from comments_scraper.src import get_sentiment
-from comments_scraper.src import push_to_db
+# from comments_scraper.src import push_to_db
