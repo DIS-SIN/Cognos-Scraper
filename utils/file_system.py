f03efdb4a6f59aa1d45800aedfd718ac217b758c
unknown <38829219+deepklim@users.noreply.github.com>
2019-06-13 15:31:54 -0400

    Initial upload

---
+++ utils/file_system.py
@@ -0,0 +1,26 @@
+import glob
+import os
+import time
+
+
+def check_file_exists(filename):
+	"""Check every 5 seconds if file exists in current working directory; send
+	email on error.
+	"""
+	ctr = 0
+	while not os.path.isfile(filename):
+		if ctr == 60:
+			return False
+		time.sleep(5)
+		ctr += 1
+	return True
+
+
+def delete_files_of_this_ilk(ilk):
+	"""Delete 'Foo.xls', 'Foo (1).xls', etc. from current working directory."""
+	files = glob.glob('{0}*.xls'.format(ilk))
+	for file in files:
+		try:
+			os.remove(file)
+		except FileNotFoundError:
+			pass
