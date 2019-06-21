37c4c35d336cd12da9766827bad39aa0ba3d7d5a
unknown <38829219+deepklim@users.noreply.github.com>
2019-06-21 11:41:40 -0400

    Add virtual viewport to trick Cognos

--- comments_scraper/src/download_files.py
+++ comments_scraper/src/download_files.py
@@ -2,6 +2,7 @@
 as CSVs from Cognos.
 """
 import os
+from pyvirtualdisplay import Display
 from selenium import webdriver
 from config import shared_directories
 from utils.file_system import check_file_exists, delete_files_of_this_ilk
@@ -16,35 +17,41 @@ assert PASSWORD is not None, 'Missing PASSWORD'
 os.chdir(shared_directories.DOWNLOADS_DIR)
 delete_files_of_this_ilk('Comments')
 delete_files_of_this_ilk('Overall Satisfaction')
-print('1/7: Previous files deleted.')
+print('1/8: Previous files deleted.')
+
+# Open virtual viewport
+display = Display(visible=0, size=(1920, 1080))
+display.start()
+print('2/8: Virtual viewport opened.')
 
 # Open controlled browser
 browser = webdriver.Chrome()
-print('2/7: Browser opened.')
+print('3/8: Browser opened.')
 
 # Navigate to Cognos and login
 browser.get(os.environ.get('LOGIN_URL'))
 browser.find_element_by_id('CAMUsername').send_keys(USERNAME)
 browser.find_element_by_id('CAMPassword').send_keys(PASSWORD)
 browser.find_element_by_id('cmdOK').click()
-print('3/7: Logged in to Cognos.')
+print('4/8: Logged in to Cognos.')
 
 # Download comments
 browser.get(os.environ.get('COMMENTS_URL'))
 os.chdir(shared_directories.DOWNLOADS_DIR)
 assert check_file_exists('Comments.xls'), 'Comments download unsuccessful'
-print('4/7: Comments downloaded.')
+print('5/8: Comments downloaded.')
 
 # Download overall satisfaction
 browser.get(os.environ.get('OVERALL_SATISFACTION_URL'))
 os.chdir(shared_directories.DOWNLOADS_DIR)
 assert check_file_exists('Overall Satisfaction.xls'), 'Overall Satisfaction download unsuccessful'
-print('5/7: Overall satisfaction downloaded.')
+print('6/8: Overall satisfaction downloaded.')
 
 # Logout
 browser.find_elements_by_css_selector('#_NS_logOnOff td')[0].click()
-print('6/7: Logged out.')
+print('7/8: Logged out of Cognos.')
 
 # End app
 browser.quit()
-print('7/7: App ended.')
+display.stop()
+print('8/8: App ended.')
