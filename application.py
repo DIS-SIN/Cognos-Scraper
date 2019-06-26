c8b7ccd7bfb8ae51d8445fdeefb56ef79e53ce24
unknown <38829219+deepklim@users.noreply.github.com>
2019-06-25 20:00:38 -0400

    Add logging

--- application.py
+++ application.py
@@ -1,8 +1,17 @@
-from comments_scraper.src import download_files
-# from comments_scraper.src import munge_comments
-# from comments_scraper.src import get_sentiment
-# from comments_scraper.src import push_to_db
+def main():
+	# Comments scraper
+	from comments_scraper.src import download_files
+	from comments_scraper.src import munge_comments
+	from comments_scraper.src import get_sentiment
+	from comments_scraper.src import push_to_db
+	
+	# Registrations scraper
+	from registrations_scraper.src import download_files
+	from registrations_scraper.src import geocode_registrations
+	from registrations_scraper.src import push_to_db
 
-# from registrations_scraper.src import download_files
-# from registrations_scraper.src import geocode_registrations
-# from registrations_scraper.src import push_to_db
+
+if __name__ == '__main__':
+	import logging.config
+	logging.config.fileConfig(fname='config/logging.ini')
+	main()
