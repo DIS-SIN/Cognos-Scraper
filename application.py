9f87a685d89458d9b45c1700a30f10f225f7f193
unknown <38829219+deepklim@users.noreply.github.com>
2019-06-26 14:13:48 -0400

    Check for presence of required files and dirs

--- application.py
+++ application.py
@@ -1,3 +1,8 @@
+import logging
+import logging.config
+import os
+
+
 def main():
 	# Comments scraper
 	from comments_scraper.src import download_files
@@ -11,11 +16,40 @@ def main():
 	from registrations_scraper.src import push_to_db
 
 
+def check_for_pickles():
+	"""Check if pickles required for memoization are present."""
+	check_sentiment_dict = os.path.isfile('./comments_scraper/pickles/sentiment_dict.pickle')
+	check_geo_dict = os.path.isfile('./registrations_scraper/pickles/geo_dict.pickle')
+	if not check_sentiment_dict or not check_geo_dict:
+		return False
+	return True
+
+
+def check_for_processed_dirs():
+	"""Check if dirs into which outputs will be saved are present."""
+	check_comments_processed = os.path.isdire('./comments_scraper/processed')
+	check_registrations_processed = os.path.isdire('./registrations_scraper/processed')
+	if not check_comments_processed or not check_registrations_processed:
+		return False
+	return True
+
+
 if __name__ == '__main__':
-	import logging.config
 	# Instantiate parent logger
 	logging.config.fileConfig(fname='config/logging.ini')
 	logger = logging.getLogger(__name__)
+	
+	# Check for pickles are processed dirs
+	pickles = check_for_pickles()
+	if not pickles:
+		logger.critical('Failure: Missing pickles.')
+		exit()
+	dirs = check_for_processed_dirs()
+	if not dirs:
+		logger.critical('Failure: Missing processed directories.')
+		exit()
+	
+	# Run app
 	try:
 		main()
 	except Exception:
