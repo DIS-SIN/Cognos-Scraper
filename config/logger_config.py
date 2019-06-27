7ab1c12f0f1090f9895aced6d4e113b6ec60a2f6
unknown <38829219+deepklim@users.noreply.github.com>
2019-06-26 23:48:44 -0400

    Add custom handler and formatter for Slack

--- config/logger_config.py
+++ config/logger_config.py
@@ -1,4 +1,38 @@
+import json
+import logging
 import os
+import requests
+import time
+
+assert os.environ.get('SLACK_BOT_URL', None) is not None, 'Missing environ var SLACK_BOT_URL.'
+
+class SlackHandler(logging.Handler):
+	"""Custom handler to store log record in JSON and send in POST request."""
+	def emit(self, record):
+		"""Set headers and URL."""
+		log_entry = self.format(record)
+		url = os.environ.get('SLACK_BOT_URL')
+		return requests.post(url, log_entry, headers={'Content-type': 'application/json'}).content
+
+
+class SlackFormatter(logging.Formatter):
+	"""Custom formatter to structure JSON object as required by Slack."""
+	def __init__(self):
+		super(SlackFormatter, self).__init__()
+	
+	def format(self, record):
+		"""Unpack record object and assemble into string."""
+		asc_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(record.created)))
+		level_name = record.levelname
+		name = record.name
+		message = record.msg
+		return_string = f'{asc_time} - {level_name} - {name} - {message}'
+		data = {'text': return_string}
+		return json.dumps(data)
+
+
+# Add custom handler to logging library
+logging.handlers.SlackHandler = SlackHandler
 
 logger_dict = {
 	'version': 1,
@@ -6,6 +40,9 @@ logger_dict = {
 		'formatter': {
 			'format': '%(asctime)s - %(levelname)s - %(name)s - %(message)s',
 			'datefmt': '%Y-%m-%d %H:%M:%S'
+		},
+		'slackFormatter': {
+			'()': SlackFormatter
 		}
 	},
 	'handlers': {
@@ -21,12 +58,17 @@ logger_dict = {
 			'level': 'INFO',
 			'formatter': 'formatter',
 			'stream': 'ext://sys.stdout'
+		},
+		'slackHandler': {
+			'class': 'logging.handlers.SlackHandler',
+			'level': 'INFO',
+			'formatter': 'slackFormatter'
 		}
 	},
 	'loggers': {
 		# Root logger identified with empty string
 		'': {
-			'handlers': ['fileHandler', 'stdOutHandler'],
+			'handlers': ['fileHandler', 'stdOutHandler', 'slackHandler'],
 			'level': 'NOTSET'
 		}
 	}
