55a9bc0a12f75ff580aa1b5af4ff47bb3781eb61
unknown <38829219+deepklim@users.noreply.github.com>
2019-06-28 19:03:37 -0400

    Add func to perform check

--- comments_scraper/src/check_unique_vals.py
+++ comments_scraper/src/check_unique_vals.py
@@ -17,25 +17,22 @@ comments = pd.read_csv('Comments.xls', sep='\t', index_col=False, encoding='utf_
 if not comments.shape[0] > 0:
 	logger.critical('Failure: Comments.xls is empty.')
 	exit()
-
 logger.info('1/4: Data imported.')
 
-# Check column 'quarter'
-quarter = comments['quarter'].unique()
-for elem in quarter:
-	if elem not in unique_vals.QUARTER:
-		logger.critical('Failure: Unknown value \'{0}\' in latest Cognos extract.'.format(elem))
-		exit()
 
+def _check_column(col_vals, target_vals):
+	"""Compare a column's unique values to target set."""
+	for elem in col_vals:
+		if elem not in target_vals:
+			logger.critical('Failure: Unknown value \'{0}\' in latest Cognos extract.'.format(elem))
+			exit()
+
+# Check column 'quarter'
+_check_column(comments['quarter'].unique(), unique_vals.QUARTER)
 logger.info('2/4: Column \'quarter\' verified.')
 
-# Check column 'quarter'
-original_question = comments['original_question'].unique()
-for elem in original_question:
-	if elem not in unique_vals.ORIGINAL_QUESTION:
-		logger.critical('Failure: Unknown value \'{0}\' in latest Cognos extract.'.format(elem))
-		exit()
-
+# Check column 'original_question'
+_check_column(comments['original_question'].unique(), unique_vals.ORIGINAL_QUESTION)
 logger.info('3/4: Column \'original_question\' verified.')
 
 logger.info('4/4: Check complete: No unknown values.')
