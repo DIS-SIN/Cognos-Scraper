c556ab616bb597d93080ca8829085df7ea624a53
unknown <38829219+deepklim@users.noreply.github.com>
2019-06-28 19:15:15 -0400

    Add functionality to check unique vals of key columns

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ def main():
 	# Comments scraper
 	from comments_scraper.src import download_files
 	from comments_scraper.src import compare_row_counts
+	from comments_scraper.src import check_unique_vals
 	from comments_scraper.src import munge_comments
 	from comments_scraper.src import get_sentiment
 	from comments_scraper.src import push_to_db
@@ -15,6 +16,7 @@ def main():
 	# Registrations scraper
 	from registrations_scraper.src import download_files
 	from registrations_scraper.src import compare_row_counts
+	from registrations_scraper.src import check_unique_vals
 	from registrations_scraper.src import geocode_registrations
 	from registrations_scraper.src import push_to_db
 
