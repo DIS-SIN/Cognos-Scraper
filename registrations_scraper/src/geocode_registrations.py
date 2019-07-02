c0eb820f080aaebb163ba04da3432ad57d86d825
unknown <38829219+deepklim@users.noreply.github.com>
2019-07-02 14:58:02 -0400

    Ensure new columns contain valid coördinates

--- registrations_scraper/src/geocode_registrations.py
+++ registrations_scraper/src/geocode_registrations.py
@@ -7,6 +7,7 @@ import requests
 import urllib
 import pandas as pd
 from config import shared_directories
+from utils.utils import _check_col_is_lat_long
 from registrations_scraper.config import directories
 
 # Instantiate logger
@@ -113,6 +114,14 @@ regs['offering_lng'] = regs.apply(lambda x: get_lat_lng(x['offering_city'], x['o
 # Add learner latitude and longitude
 regs['learner_lat'] = regs.apply(lambda x: get_lat_lng(x['learner_city'], x['learner_province'])['lat'], axis=1)
 regs['learner_lng'] = regs.apply(lambda x: get_lat_lng(x['learner_city'], x['learner_province'])['lng'], axis=1)
+
+# Ensure new columns contain valid coördinates
+if not _check_col_is_lat_long(regs['offering_lat'].unique()) and
+	   _check_col_is_lat_long(regs['offering_lng'].unique()) and
+	   _check_col_is_lat_long(regs['learner_lat'].unique()) and
+	   _check_col_is_lat_long(regs['learner_lng'].unique()):
+	logger.critical('Failure: Invalid coördinates.')
+    exit()
 
 logger.debug('3/5: New columns created.')
 
--- utils/utils.py
+++ utils/utils.py
@@ -7,3 +7,19 @@ def _check_column(col_vals, target_vals):
 		if elem not in target_vals:
 			logger.critical('Failure: Unknown value \'{0}\' in latest Cognos extract.'.format(elem))
 			exit()
+
+
+def _check_col_is_lat_long(my_list):
+	"""Ensure values are valid coördinates (i.e. between -180 and 180) or
+	a MySQL null (i.e. '\\N').
+	"""
+	for val in my_list:
+		try:
+			val_float = float(val)
+		except ValueError:
+			if val != '\\N':
+				return False
+		else:
+			if not (-180 <= val_float <= 180):
+				return False
+	return True
