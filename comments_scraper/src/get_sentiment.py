b5339cd15af4e21ef6e22dda05ef47d57d1b56cb
unknown <38829219+deepklim@users.noreply.github.com>
2019-08-20 21:51:54 -0400

    Handle known exception rather than all exceptions

--- comments_scraper/src/get_sentiment.py
+++ comments_scraper/src/get_sentiment.py
@@ -5,6 +5,7 @@ import os
 import pickle
 import time
 from google.cloud import language
+from google.api_core.exceptions import InvalidArgument
 import pandas as pd
 from utils.utils import _check_col_in_valid_range
 from comments_scraper.config import directories
@@ -41,12 +42,12 @@ ctr = 0
 api_ctr = 0
 
 
-def get_sentiment_score(survey_id, original_question, short_question, text_answer):
+def get_sentiment_score(survey_id, original_question, short_question, text_answer, overall_satisfaction):
 	"""Pass text to API, return its sentiment score, and memoize results."""
 	global ctr
 	global api_ctr
 	ctr += 1
-	# Log ctr every 1000 comments
+	# Log ctr every 10k comments
 	if ctr % 10_000 == 0:
 		logger.debug('Finished {0} comments.'.format(ctr))
 	
@@ -68,13 +69,24 @@ def get_sentiment_score(survey_id, original_question, short_question, text_answe
 	if api_ctr % 590 == 0:
 		time.sleep(60)
 	
-	document = language.types.Document(content=text_answer,
-									   type=language.enums.Document.Type.PLAIN_TEXT)
-	sentiment = client.analyze_sentiment(document=document).document_sentiment
-	# Adjust sentiment scores from real numbers in [-1, 1] to integers in [1, 5]
-	# Cast to Decimal then back to int to prevent floating point rounding errors
-	sent = int(round(Decimal(str((sentiment.score * 2) + 3))))
-	mag = sentiment.magnitude
+	try:
+		document = language.types.Document(content=text_answer,
+										   type=language.enums.Document.Type.PLAIN_TEXT)
+		sentiment = client.analyze_sentiment(document=document).document_sentiment
+		# Adjust sentiment scores from real numbers in [-1, 1] to integers in [1, 5]
+		# Cast to Decimal then back to int to prevent floating point rounding errors
+		sent = int(round(Decimal(str((sentiment.score * 2) + 3))))
+		mag = sentiment.magnitude
+	# Comments occasionally so badly written the API can't identify the language
+	except InvalidArgument as e:
+		logger.info('API failed to process: {0}'.format(text_answer))
+		# Default to overall_satisfaction
+		if overall_satisfaction == '\\N':
+			sent = '\\N'
+			mag = '\\N'
+		else:
+			sent = int(float(overall_satisfaction) / 2)
+			mag = '\\N'
 	
 	# Memoize and return result
 	result = (sent, mag)
@@ -82,7 +94,7 @@ def get_sentiment_score(survey_id, original_question, short_question, text_answe
 	return result
 
 
-api_results = df.apply(lambda x: get_sentiment_score(x['survey_id'], x['original_question'], x['short_question'], x['text_answer']),
+api_results = df.apply(lambda x: get_sentiment_score(x['survey_id'], x['original_question'], x['short_question'], x['text_answer'], x['overall_satisfaction']),
 					   axis=1,					# Apply to each row
 					   raw=False,				# Pass each cell individually as not using NumPy
 					   result_type='expand')	# Return DataFrame rather than Series of tuples
