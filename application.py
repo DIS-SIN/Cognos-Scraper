6c565d7c150d477ebba38228a811ce1dd49eddd1
unknown <38829219+deepklim@users.noreply.github.com>
2019-08-21 15:32:14 -0400

    Add new module to main

--- application.py
+++ application.py
@@ -6,17 +6,18 @@ from config.logger_config import logger_dict
 
 def main():
 	# Comments scraper
-	# from comments_scraper.src import download_files
-	# from comments_scraper.src import compare_row_counts
-	# from comments_scraper.src import check_unique_vals
-	# from comments_scraper.src import munge_comments
-	# from comments_scraper.src import get_sentiment
-	# from comments_scraper.src import push_to_db
+	from comments_scraper.src import download_files
+	from comments_scraper.src import compare_row_counts
+	from comments_scraper.src import check_unique_vals
+	from comments_scraper.src import munge_comments
+	from comments_scraper.src import get_sentiment
+	from comments_scraper.src import push_to_db
 	
 	# Registrations scraper
 	from registrations_scraper.src import download_files
 	from registrations_scraper.src import compare_row_counts
 	from registrations_scraper.src import check_unique_vals
+	from registrations_scraper.src import munge_registrations
 	from registrations_scraper.src import geocode_registrations
 	from registrations_scraper.src import push_to_db
 	
