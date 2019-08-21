efc6ec37362dad273ee479d5ede104bbe52e9729
unknown <38829219+deepklim@users.noreply.github.com>
2019-08-21 11:38:49 -0400

    Add validation for FR provinces

--- registrations_scraper/src/check_unique_vals.py
+++ registrations_scraper/src/check_unique_vals.py
@@ -18,50 +18,62 @@ regs = pd.read_csv('LSR Mini.xls', sep='\t', index_col=False, encoding='utf_16_l
 if not regs.shape[0] > 0:
 	logger.critical('Failure: LSR Mini.xls is empty.')
 	exit()
-logger.debug('1/13: Data imported.')
+logger.debug('1/16: Data imported.')
 
 # Check column 'business_type'
 _check_column(logger, regs['business_type'].unique(), unique_vals.BUSINESS_TYPE)
-logger.debug('2/13: Column \'business_type\' verified.')
+logger.debug('2/16: Column \'business_type\' verified.')
 
 # Check column 'month_en'
 _check_column(logger, regs['month_en'].unique(), unique_vals.MONTH_EN)
-logger.debug('3/13: Column \'month_en\' verified.')
+logger.debug('3/16: Column \'month_en\' verified.')
+
+# Check column 'month_fr'
+_check_column(logger, regs['month_fr'].unique(), unique_vals.MONTH_FR)
+logger.debug('4/16: Column \'month_fr\' verified.')
 
 # Check column 'offering_status'
 _check_column(logger, regs['offering_status'].unique(), unique_vals.OFFERING_STATUS)
-logger.debug('4/13: Column \'offering_status\' verified.')
+logger.debug('5/16: Column \'offering_status\' verified.')
 
 # Check column 'offering_language'
 _check_column(logger, regs['offering_language'].unique(), unique_vals.OFFERING_LANGUAGE)
-logger.debug('5/13: Column \'offering_language\' verified.')
+logger.debug('6/16: Column \'offering_language\' verified.')
 
 # Check column 'offering_region_en'
-_check_column(logger, regs['offering_region_en'].unique(), unique_vals.OFFERING_REGION_EN)
-logger.debug('6/13: Column \'offering_region_en\' verified.')
+_check_column(logger, regs['offering_region_en'].unique(), unique_vals.REGION_EN)
+logger.debug('7/16: Column \'offering_region_en\' verified.')
 
 # Check column 'offering_region_fr'
-_check_column(logger, regs['offering_region_fr'].unique(), unique_vals.OFFERING_REGION_FR)
-logger.debug('7/13: Column \'offering_region_fr\' verified.')
+_check_column(logger, regs['offering_region_fr'].unique(), unique_vals.REGION_FR)
+logger.debug('8/16: Column \'offering_region_fr\' verified.')
 
 # Check column 'offering_province_en'
-_check_column(logger, regs['offering_province_en'].unique(), unique_vals.OFFERING_PROVINCE_EN)
-logger.debug('8/13: Column \'offering_province_en\' verified.')
+_check_column(logger, regs['offering_province_en'].unique(), unique_vals.PROVINCE_EN)
+logger.debug('9/16: Column \'offering_province_en\' verified.')
 
-# Check column 'learner_province'
-_check_column(logger, regs['learner_province'].unique(), unique_vals.LEARNER_PROVINCE)
-logger.debug('9/13: Column \'learner_province\' verified.')
+# Check column 'offering_province_fr'
+_check_column(logger, regs['offering_province_fr'].unique(), unique_vals.PROVINCE_FR)
+logger.debug('10/16: Column \'offering_province_fr\' verified.')
+
+# Check column 'learner_province_en'
+_check_column(logger, regs['learner_province_en'].unique(), unique_vals.PROVINCE_EN)
+logger.debug('11/16: Column \'learner_province_en\' verified.')
+
+# Check column 'learner_province_fr'
+_check_column(logger, regs['learner_province_fr'].unique(), unique_vals.PROVINCE_FR)
+logger.debug('12/16: Column \'learner_province_fr\' verified.')
 
 # Check column 'reg_status'
 _check_column(logger, regs['reg_status'].unique(), unique_vals.REG_STATUS)
-logger.debug('10/13: Column \'reg_status\' verified.')
+logger.debug('13/16: Column \'reg_status\' verified.')
 
 # Check column 'no_show'
 _check_column(logger, regs['no_show'].unique(), unique_vals.NO_SHOW)
-logger.debug('11/13: Column \'no_show\' verified.')
+logger.debug('14/16: Column \'no_show\' verified.')
 
 # Check column 'learner_language'
 _check_column(logger, regs['learner_language'].unique(), unique_vals.LEARNER_LANGUAGE)
-logger.debug('12/13: Column \'learner_language\' verified.')
+logger.debug('15/16: Column \'learner_language\' verified.')
 
-logger.debug('13/13: Check complete: No unknown values.')
+logger.debug('16/16: Check complete: No unknown values.')
